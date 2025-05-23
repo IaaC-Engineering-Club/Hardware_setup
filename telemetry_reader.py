05fcd8f432aefd6e5ffe8069ba5bf9dd7017b3f1
Salvador Cantuarias <salvador.cantuarias@students.iaac.net>
2025-05-23 18:53:09 +0200

    finished json file containing boat data for localization. Now we need to connect gps and send that data

--- telemetry_reader.py
+++ telemetry_reader.py
@@ -17,7 +17,27 @@ while True:
     if not msg:
         continue
 
+    msg_data = {
+        'GPS_RAW_INT':{
+        'time_usec': msg.time_usec,
+        'lat': msg.lat / 1e7,
+        'lon': msg.lon / 1e7,
+        'alt': msg.alt / 1000,
+        },
+        'ATTITUDE':{
+            'IMU': msg.roll,
+            'pitch': msg.pitch,
+            'yaw': msg.yaw
+        }
+
+    }
+
     if msg.get_type() == 'GPS_RAW_INT':
+        print(f"GPS | Lat: {msg_data['GPS_RAW_INT']['lat']}, Lon: {msg_data['GPS_RAW_INT']['lon']}, Alt: {msg_data['GPS_RAW_INT']['alt']} m")
+    elif msg.get_type() == 'ATTITUDE':
+        print(f"IMU | Roll: {msg_data['ATTITUDE']['IMU']:.2f}, Pitch: {msg_data['ATTITUDE']['pitch']:.2f}, Yaw: {msg_data['ATTITUDE']['yaw']:.2f}")
+
+'''    if msg.get_type() == 'GPS_RAW_INT':
         print(f"GPS | Lat: {msg.lat / 1e7}, Lon: {msg.lon / 1e7}, Alt: {msg.alt / 1000} m")
     elif msg.get_type() == 'ATTITUDE':
-        print(f"IMU | Roll: {msg.roll:.2f}, Pitch: {msg.pitch:.2f}, Yaw: {msg.yaw:.2f}")
+        print(f"IMU | Roll: {msg.roll:.2f}, Pitch: {msg.pitch:.2f}, Yaw: {msg.yaw:.2f}")'''
